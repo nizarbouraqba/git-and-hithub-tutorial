2bd31e7ccd6c4638dbe355e8973132bbb56d0d9c
nouhaila <nouha.nouhailoux@gmail.com>
2025-05-05 21:55:12 +0100

    ajouter validation.php pour lan ar

---
+++ resources/lang/ar/validation.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    'accepted' => 'يجب قبول :attribute.',
+    'active_url' => ':attribute ليس رابطاً صحيحاً.',
+    'after' => 'يجب أن يكون :attribute تاريخاً بعد :date.',
+    'alpha' => 'يجب أن يحتوي :attribute على أحرف فقط.',
+    'in' => ':attribute غير صالح.',
+    'min' => [
+        'string' => 'يجب أن لا يقل :attribute عن :min حروف.',
+    ],
+    'unique' => ':attribute مستخدم من قبل.',
+
+    'required' => 'حقل :attribute مطلوب.',
+    'email' => 'يجب أن يكون :attribute بريداً إلكترونياً صالحاً.',
+    'max' => [
+        'string' => 'يجب ألا يتجاوز :attribute :max حرفاً.',
+    ],
+    'confirmed' => 'تأكيد :attribute غير متطابق.',
+    'boolean' => 'يجب أن تكون قيمة :attribute صحيحة أو خاطئة.',
+
+    'attributes' => [
+        'first_name' => 'الاسم الأول',
+        'last_name' => 'اسم العائلة',
+        'email' => 'البريد الإلكتروني',
+        'phone' => 'رقم الهاتف',
+        'gender' => 'الجنس',
+        'city' => 'المدينة',
+        'province' => 'الجهة',
+        'motivation' => 'الدافع',
+        'education_level' => 'المستوى الدراسي',
+        'field_of_study' => 'مجال الدراسة',
+        'interests' => 'الاهتمامات',
+        'hear_about_us' => 'مصدر المعرفة بنا',
+        'receive_newsletter' => 'النشرة الإخبارية',
+        'data_consent' => 'الموافقة على استخدام البيانات',
+        'values_consent' => 'الموافقة على القيم',
+    ],
+
+];
