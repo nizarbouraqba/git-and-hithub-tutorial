a7c2796d935a89c18d0c11d46a6ef9edbeee1ce6
nouhaila <nouha.nouhailoux@gmail.com>
2025-05-05 21:41:12 +0100

    ajouter file validation.php

---
+++ resources/lang/fr/validation.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    'accepted' => 'Le champ :attribute doit être accepté.',
+    'active_url' => 'Le champ :attribute n’est pas une URL valide.',
+    'after' => 'Le champ :attribute doit être une date postérieure au :date.',
+    'alpha' => 'Le champ :attribute ne peut contenir que des lettres.',
+    'in' => 'Le champ :attribute sélectionné est invalide.',
+    'min' => [
+        'string' => 'Le champ :attribute doit contenir au moins :min caractères.',
+    ],
+    'unique' => 'Le champ :attribute est déjà utilisé.',
+
+    'required' => 'Le champ :attribute est obligatoire.',
+    'email' => 'Le champ :attribute doit être une adresse email valide.',
+    'max' => [
+        'string' => 'Le champ :attribute ne peut pas contenir plus de :max caractères.',
+    ],
+    'confirmed' => 'La confirmation de :attribute ne correspond pas.',
+    'boolean' => 'Le champ :attribute doit être vrai ou faux.',
+
+    'attributes' => [
+        'first_name' => 'prénom',
+        'last_name' => 'nom',
+        'email' => 'adresse email',
+        'phone' => 'téléphone',
+        'gender' => 'genre',
+        'city' => 'ville',
+        'province' => 'région',
+        'motivation' => 'motivation',
+        'education_level' => 'niveau d\'études',
+        'field_of_study' => 'domaine d\'études',
+        'interests' => 'centres d’intérêt',
+        'hear_about_us' => 'source de connaissance',
+        'receive_newsletter' => 'newsletter',
+        'data_consent' => 'consentement des données',
+        'values_consent' => 'consentement aux valeurs',
+    ],
+
+];
